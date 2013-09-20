01245eb9f9868e71f621f4cc20408167bc17c4f7
Jim Safley <jimsafley@gmail.com>
2013-09-20 14:34:51 -0400

    Make each 911 weblog an item.

--- lib/Sept11/Import/Strategy/Sept11Collections911Weblogs.php
+++ lib/Sept11/Import/Strategy/Sept11Collections911Weblogs.php
@@ -22,7 +22,6 @@ class Sept11_Import_Strategy_Sept11Collections911Weblogs
         $collectionOmekaId = $this->_insertCollection();
         
         // Create all the ZIP archives and directory trees.
-        $zipFiles = array();
         foreach (scandir($this->_collectionSept11['COLLECTION_FOLDER_NAME']) as $dirName) {
             if (in_array($dirName, array('.', '..'))) {
                 continue;
@@ -39,7 +38,7 @@ class Sept11_Import_Strategy_Sept11Collections911Weblogs
             $dirTree = shell_exec("tree -a -I read_me.txt $zipDirName");
             exec("zip -r $zipArchivePath.zip ./$zipDirName");
             
-            $zipFiles[] = array(
+            $zipFile = array(
                 'source' => "$zipArchivePath.zip", 
                 'metadata' => array(
                     'Dublin Core' => array(
@@ -47,42 +46,40 @@ class Sept11_Import_Strategy_Sept11Collections911Weblogs
                     ),
                 ), 
             );
-        }
-        
-        // Build the pseudo object array.
-        $object = array(
-            'OBJECT_ID' => 0, 
-            'CONTRIBUTOR_ID' => $this->_collectionSept11['CONTRIBUTOR_ID'], 
-            'STATUS_ID' => $this->_collectionSept11['STATUS_ID'], 
-            'CONSENT_ID' => 5, 
-            'SOURCE_ID' => 6, 
-            'OBJECT_MEDIA_TYPE_ID' => 13, 
-            'OBJECT_TITLE' => $this->_collectionSept11['COLLECTION_TITLE'], 
-            'OBJECT_DESC' => $this->_collectionSept11['COLLECTION_DESC'], 
-            'OBJECT_POSTING' => 'unknown', 
-            'OBJECT_COPYRIGHT' => 'unknown', 
-            'OBJECT_ORIG_NAME' => null, 
-            'OBJECT_AUTHOR_CREATE' => null, 
-            'OBJECT_AUTHOR_DESCRIBE' => null, 
-            'OBJECT_DATE_ENTERED' => null, 
-            'OBJECT_IP_SOURCE' => null, 
-            'OBJECT_ANNOTATION' => null, 
-            'OBJECT_NOTES' => null, 
-        );
-        
-        // Insert the item, attaching the Zip archive.
-        $metadata = array();
-        $elementTexts = array();
-        $fileMetadata = array(
-            'file_transfer_type' => 'Filesystem', 
-            'files' => $zipFiles, 
-        );
-        
-        $itemId = $this->_insertItem($collectionOmekaId, $object, 
-                                     $metadata, $elementTexts, $fileMetadata);
-        
-        // Delete the temporary Zip archive.
-        foreach ($zipFiles as $zipFile) {
+            
+            // Build the pseudo object array.
+            $object = array(
+                'OBJECT_ID' => 0, 
+                'CONTRIBUTOR_ID' => $this->_collectionSept11['CONTRIBUTOR_ID'], 
+                'STATUS_ID' => $this->_collectionSept11['STATUS_ID'], 
+                'CONSENT_ID' => 5, 
+                'SOURCE_ID' => 6, 
+                'OBJECT_MEDIA_TYPE_ID' => 13, 
+                'OBJECT_TITLE' => basename($zipArchivePath, '.zip'), 
+                'OBJECT_DESC' => null, 
+                'OBJECT_POSTING' => 'unknown', 
+                'OBJECT_COPYRIGHT' => 'unknown', 
+                'OBJECT_ORIG_NAME' => null, 
+                'OBJECT_AUTHOR_CREATE' => null, 
+                'OBJECT_AUTHOR_DESCRIBE' => null, 
+                'OBJECT_DATE_ENTERED' => null, 
+                'OBJECT_IP_SOURCE' => null, 
+                'OBJECT_ANNOTATION' => null, 
+                'OBJECT_NOTES' => null, 
+            );
+            
+            // Insert the item, attaching the Zip archive.
+            $metadata = array();
+            $elementTexts = array();
+            $fileMetadata = array(
+                'file_transfer_type' => 'Filesystem', 
+                'files' => $zipFile, 
+            );
+            
+            $itemId = $this->_insertItem($collectionOmekaId, $object, 
+                                         $metadata, $elementTexts, $fileMetadata);
+            
+            // Delete the temporary Zip archive.
             unlink($zipFile['source']);
         }
     }
